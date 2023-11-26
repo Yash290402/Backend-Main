b97199547becbc3d51018547cc7658b683ab84c3
Yash290402 <yash56774@gmail.com>
2023-11-26 15:23:13 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000)
+        console.log(`Server started on port ${process.env.PORT}`);
+    })
+
+    .catch((err) => {
+        console.log("MONGO ERROR: " + err);
+    })
 
 
 
---
+++ src/utils/APIResponse.js
@@ -0,0 +1,8 @@
+class APIResponse {
+    constructor(statusCode, data, message = "Sucesss") {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
